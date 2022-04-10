dc27e9b5841e7bc3524f9e181972f9e57d96c1ae
Chiara Maggio - UdeSA <88746975+ChiaraMaggio@users.noreply.github.com>
2022-04-10 14:57:31 -0300

    ruta de login + vistas formularios

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -2,6 +2,9 @@ let usersController = {
     register: function (req, res) {
         return res.render("register");
     },
+    login: function (req, res) {
+        return res.render("login");
+    },
     profile: function (req, res) {
         return res.render("profile");
     },
--- routes/users.js
+++ routes/users.js
@@ -8,4 +8,6 @@ router.get("/edit", usersController.profileEdit)
 
 router.get("/register", usersController.register);
 
+router.get("/login", usersController.login)
+
 module.exports = router;
