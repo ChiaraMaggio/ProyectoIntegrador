5f9f8135486fb0b6adf948b1fc0d52b32e7c0e78
Chiara Maggio - UdeSA <88746975+ChiaraMaggio@users.noreply.github.com>
2022-04-11 11:36:30 -0300

    db usuario, vista profile

--- controllers/usersController.js
+++ controllers/usersController.js
@@ -1,3 +1,5 @@
+const usuario = require("../db/usuario");
+
 let usersController = {
     register: function (req, res) {
         return res.render("register");
@@ -6,7 +8,7 @@ let usersController = {
         return res.render("login");
     },
     profile: function (req, res) {
-        return res.render("profile");
+        return res.render("profile", {usuario: usuario.lista[0].nombreDeUsuario, foto: usuario.lista[0].fotoDePerfil, productos: usuario.lista[0].cantidadDeProductos, seguidores: usuario.lista[0].cantidadDeSeguidores, comentarios: usuario.lista[0].comentarios});
     },
     profileEdit: function (req, res) {
         return res.render("profile-edit");
--- db/usuario.js
+++ db/usuario.js
@@ -1,3 +1,41 @@
 let usuario = {
-    
-}
+    lista: [
+        {
+            nombreDeUsuario: "Hugo Vaca Guzmán",
+            fotoDePerfil: "/images/users/HugoVacaGuzman.jpg", 
+            cantidadDeProductos: "500 productos",
+            cantidadDeSeguidores: "7690 seguidores",
+            comentarios: "1577 comentarios"
+        },
+        {
+            nombreDeUsuario: "Alberto Denjam",
+            fotoDePerfil: "/images/users/HugoVacaGuzman.jpg", 
+            cantidadDeProductos: "678 productos",
+            cantidadDeSeguidores: "4670 seguidores",
+            comentarios: "2089 comentarios"
+        },
+        {
+            nombreDeUsuario: "Petrona Maguna",
+            fotoDePerfil: "/images/users/HugoVacaGuzman.jpg", 
+            cantidadDeProductos: "150 productos",
+            cantidadDeSeguidores: "708 seguidores",
+            comentarios: "207 comentarios"
+        },
+        {
+            nombreDeUsuario: "Olga Maggio",
+            fotoDePerfil: "/images/users/HugoVacaGuzman.jpg", 
+            cantidadDeProductos: "650 productos",
+            cantidadDeSeguidores: "5900 seguidores",
+            comentarios: "1340 comentarios"
+        },
+        {
+            nombreDeUsuario: "Bernarda Teperman",
+            fotoDePerfil: "/images/users/HugoVacaGuzman.jpg", 
+            cantidadDeProductos: "322 productos",
+            cantidadDeSeguidores: "6780 seguidores",
+            comentarios: "679 comentarios"
+        },
+    ]
+};
+
+module.exports = usuario;
