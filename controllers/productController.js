4f04af689e92869209b36b055dba22ecaf197ee3
TMagu <tmaguna@udesa.edu.ar>
2022-04-17 22:00:25 -0300

    .

--- controllers/productController.js
+++ controllers/productController.js
@@ -5,7 +5,7 @@ const comentarios = require("../db/comentarios");
 
 let productController = {
     product: function (req, res) {
-        return res.render("product", {productos:productos, usuario: comentarios, comentarios: comentarios});
+        return res.render("product", {productos:productos, comentarios: comentarios});
     },
     productAdd: function (req, res) {
         return res.render("product-add", {usuario: usuario.lista[0].nombreDeUsuario});
