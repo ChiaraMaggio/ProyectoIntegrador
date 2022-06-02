933b895d0761cf906b5660e307a232998a29a541
Chiara Maggio - UdeSA <88746975+ChiaraMaggio@users.noreply.github.com>
2022-06-02 20:30:47 -0300

    modelo usuario

---
+++ database/models/User.js
@@ -0,0 +1,60 @@
+module.exports = function (sequelize, dataTypes){
+    let alias = "Users";
+    
+    let cols = {
+        user_id:{
+            autoIncrement: true,
+            primaryKey: true,
+            type: dataTypes.INTEGER
+        },
+        user_name:{
+            allowNull: false,
+            type: dataTypes.STRING
+        },
+        user_lastname:{
+            allowNull: false,
+            type: dataTypes.STRING
+        },
+        birth_date:{
+            allowNull: false,
+            type: dataTypes.DATE
+        },
+        user_email:{
+            allowNull: false,
+            type: dataTypes.STRING
+        },
+        user_password:{
+            allowNull: false,
+            type: dataTypes.STRING
+        },
+        avatar:{
+            type: dataTypes.STRING
+        },
+        number_of_products:{
+            allowNull: false,
+            type: dataTypes.INTEGER
+        },
+        number_of_comments:{
+            allowNull: false,
+            type: dataTypes.INTEGER
+        },
+        remember_token:{
+            type: dataTypes.STRING
+        },
+        created_at:{
+            type: dataTypes.DATE 
+        },
+        updated_at:{
+            type: dataTypes.DATE
+        }
+    };
+    
+    let config = {
+        tableName: "users",
+        timeStamps: false
+    };
+
+    const User = sequelize.define(alias, cols, config);
+    
+    return Usuario;
+}
