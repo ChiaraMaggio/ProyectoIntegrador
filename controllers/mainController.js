953434b82db07fdb7f0ff589eebd6178d74d1320
Chiara Maggio - UdeSA <88746975+ChiaraMaggio@users.noreply.github.com>
2022-06-13 23:01:40 -0300

    cambios

--- controllers/mainController.js
+++ controllers/mainController.js
@@ -1,9 +1,6 @@
 let productos = require ("../db/productos"); 
 
-
 const db = require("../database/models"); /* requerimos la conexión a la base de datos y todos los modelos */
-
-//const db = require("../database/models"); /* requerimos la conexión a la base de datos y todos los modelos */
 
 let mainController = {
     main: function (req, res) {
