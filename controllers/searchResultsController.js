6c89a07179d20c0e9d61b8e35f574637fa8326b6
TMagu <tmaguna@udesa.edu.ar>
2022-06-22 13:36:10 -0300

    search-results

--- controllers/searchResultsController.js
+++ controllers/searchResultsController.js
@@ -1,9 +1,62 @@
 const productos = require ("../db/productos");
+const db = require("../database/models/index");
+const op = db.Sequelize.op
 
 let searchResultsController = {
-    searchResults: function (req, res) {
-        return res.render("search-results", {productos:productos});
-    } 
-}
+    /* searchResults: function (req, res) {
+                return res.render("search-results", {productos:productos});
+            
+    }, */
 
+    searchResultsDetail: function (req, res) {
+       let product = req.query.search;
+       let errors = {}
+
+       if (product == "") {
+        errors.message = "Este campo no puede estar vacio"
+        res.locals.errors = errors;
+        console.log(errors);
+        return res.render ("search-results.ejs");
+       }
+       else {
+            db.product.findAll({
+                where: {
+                    [Op.or]:[
+                        {product_name:{[Op.like]:"%"+ product +"%"}},
+                        {product_description:{[Op.like]:"%"+ product +"%"}},
+                    ]
+                },
+                order:[
+                    ["product_name", "ASC"]
+                ],
+                limit: 4,
+                include: [
+                    {association: "usersproducts"},
+                    {association: "productscomment"},
+                ],
+
+            }) 
+
+            .then((data) => {
+            
+                if(data != ''){
+                  return res.render('search-results.ejs', {data:data})
+                }else{
+                  errors.message = "Lo sentimos, no hay resultados para su búsqueda " ;
+                  res.locals.errors = errors ;
+                  return res.render('search-results.ejs')
+                }
+              })
+            .catch(function (error) {
+                console.log(error);                            
+            })
+         
+
+        
+       } 
+    },
+}
+ 
 module.exports = searchResultsController;
+
+/* return res.render("search-results", {productos:productos}); */
--- routes/searchResults.js
+++ routes/searchResults.js
@@ -1,9 +1,11 @@
 var express = require('express');
 const searchResultsController = require('../controllers/searchResultsController');
 var router = express.Router();
+let path = require('path');
 
 /* GET register listing. */
-router.get('/', searchResultsController.searchResults)
+router.get('/', searchResultsController.searchResultsDetail)
+
   
 
 module.exports = router;
